2c05ba0a18f8ab53adbd223e377995977dd9f6b7
tzdtwsj <yhytzdtwsj@outlook.com>
2023-11-26 21:52:02 +0800

    update

--- func.py
+++ func.py
@@ -1,6 +1,9 @@
 from subprocess import Popen, PIPE
 import os
 import threading
+import time
+import datetime
+from __main__ import emit, app
 def load_cmd_str(cmd:str):
     char_arr = list(cmd.strip())
     tmp_cmd_arr = [""]
@@ -38,84 +41,174 @@ def load_cmd_str(cmd:str):
         i = i + 1
     return tmp_cmd_arr
 
-class startinstance:
+def log(text:str,loglevel:str="INFO"):
+    if loglevel == "INFO":
+        loglevel2 = "\x1b[36mINFO\x1b[0m"
+    elif loglevel == "WARN":
+        loglevel2 = "\x1b[33mWARN\x1b[0m"
+    elif loglevel == "ERROR":
+        loglevel2 = "\x1b[31mERROR\x1b[0m"
+    elif loglevel == "FATAL":
+        loglevel2 = "\x1b[31mFATAL\x1b[0m"
+    elif loglevel == "DEBUG":
+        loglevel2 = "\x1b[34mDEBUG\x1b[0m"
+    else:
+        raise ValueError("无效的loglevel: "+loglevel)
+    print("\x1b[0m[\x1b[32m"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\x1b[0m \x1b[33mTzGamePanel\x1b[0m "+loglevel2+"] "+text)
+
+class instance:
+    cmd = None
+    cwd = None
     proc = None #进程类
-
-    status = None #进程返回的返回值
-
     stdin = None #管道
-
     stdout = None #管道
-
-    stdout2 = None #class将会使用此管道
-
+    stdout2 = []
     instance_id = None #实例id
-
-    start_success = None #是否启动进程成功
-
-    error_msg = "" #如果start_success == False，那么这个变量会有内容
-
-    #stderr = None#为了方便，stderr与stdout合并
-
     Thread_read_proc_text_to_file = None
-
-    Thread_check_proc_is_running = None
+    closed = False
+    status = 0 #关于status的解释：0-已关闭，1-运行中，2-关闭中
 
     def __init__(self,cmd:list,cwd:str,instance_id:str):
+        if self.closed:
+            raise Exception("此对象已被关闭")
+        if type(cmd) != list:
+            raise TypeError(f"cmd should be list, not {type(cmd)}")
         self.stdin = os.pipe()#os.pipe返回的是元组，元组的返回的第一个fd是读，第二个fd是写
         self.stdout = os.pipe()
-        self.stdout2 = os.pipe()
         #self.stderr = os.pipe()
+        self.cmd = cmd
+        self.cwd = cwd
         self.instance_id = instance_id
+        def read_proc_text_to_file(file,fd,self):
+            while True:
+                try:
+                    text = os.read(fd,256000)
+                except Exception as e:
+                    return
+                with open(file,"ab") as f:
+                    f.write(text)
+                self.stdout2.append(text)
+                try:
+                    with app.app_context():
+                        emit('terminal', text, to="instance_"+instance_id, namespace="/ws", broadcast=True)
+                except Exception as e:
+                    print(str(e))
+            
+        self.Thread_read_proc_text_to_file = threading.Thread(
+            target = read_proc_text_to_file,
+            kwargs = {
+                'file': os.path.abspath("data/InstanceLog/"+self.instance_id+".log"),
+                'fd': self.stdout[0],
+                'self': self
+            }
+        )
+        self.Thread_read_proc_text_to_file.setDaemon(True)
+        self.Thread_read_proc_text_to_file.start()
+
+    def start(self):
+        if self.closed:
+            raise Exception("此对象已被关闭")
+        if self.proc != None:
+            if self.proc.poll() == None:
+                return False
         try:
+            if os.path.isdir(self.cmd[0]):
+                raise Exception(f"{self.cmd[0]}: 是一个目录")
             self.proc = Popen(
-                args = cmd,#命令参数
+                args = self.cmd,#命令参数
                 shell = False,#是否由shell带动此进程
                 stdin = self.stdin[0],#标准输入
                 stdout = self.stdout[1],#标准输出
                 stderr = self.stdout[1],#标准错误输出
-                cwd = cwd
+                cwd = self.cwd
             )
         except Exception as e:
-            self.start_success = False
-            self.error_msg = e.__str__() + " 在class " + str(type(e)) + " 中"
+            os.write(self.stdout[1],("[TzGamePanel] 实例启动失败，请检查启动命令\n    启动命令：\n").encode("utf-8"))
+            os.write(self.stdout[1],("  ").encode("utf-8"))
+            for i in self.cmd:
+                os.write(self.stdout[1],(i+" ").encode("utf-8"))
+            os.write(self.stdout[1],("\n").encode("utf-8"))
+            os.write(self.stdout[1],("  启动时捕获到的异常："+str(e)+"\n").encode("utf-8"))
+            return e
+        self.status = 1
+        def ciir(self):
+            while True:
+                time.sleep(1)
+                if not self.is_running():
+                    self.status = 0
+                    os.write(self.stdout[1],("[TzGamePanel] 实例已关闭"+"\n").encode("utf-8"))
+                    return
+        threading.Thread(target=ciir,kwargs={'self':self}).start()
+        os.write(self.stdout[1],("[TzGamePanel] 实例已启动"+"\n").encode("utf-8"))
+        return True
+
+    def is_running(self):
+        if self.closed:
+            raise Exception("此对象已被关闭")
+        if self.proc == None:
+            return False
+        if self.proc.poll() == None:
+            return True
         else:
-            self.start_success = True
-            def read_proc_text_to_file(file,fd,fd2,self):
-                while True:
-                    try:
-                        text = os.read(fd,256000)
-                        with open(file,"ab") as f:
-                            f.write(text)
-                        os.write(fd2,text)
-                    except Exception:
-                        break
-                
-                def check_proc_is_running(self):
-                    while True:
-                        if self.proc.poll() != None:
-                            self.status = self.proc.poll()
-                            os.close(self.stdin[0])
-                            os.close(self.stdin[1])
-                            os.close(self.stdout[0])
-                            os.close(self.stdout[1])
-                            return
-            self.Thread_read_proc_text_to_file = threading.Thread(
-                target = read_proc_text_to_file,
-                kwargs = {
-                    'file': os.path.abspath("data/InstanceLog/"+self.instance_id+".log"),
-                    'fd': self.stdout[0],
-                    'fd2': self.stdout2[1],
-                    'self': self
-                }
-            )
-            self.Thread_check_proc_is_running = threading.Thread(
-                target = check_proc_is_running,
-                kwargs = {
-                    'self': self
-                }
-            )
-            self.Thread_read_proc_text_to_file.setDaemon(True)
-            self.Thread_check_proc_is_running.setDaemon(True)
-            self.Thread_read_proc_text_to_file.start()
-            self.Thread_check_proc_is_running.start()
+            return False
+
+    def get_return_code(self):
+        if self.closed: 
+            raise Exception("此对象已被关闭")
+        if self.proc.poll() == None:
+            return False
+        return self.proc.poll()
+
+    def stop(self,stop_cmd:str="stop"):
+        if self.closed: 
+            raise Exception("此对象已被关闭")
+        if not self.is_running():
+            return False
+        if self.status == 2:
+            return
+        self.status = 2
+        def csis(self,ct):
+            while time.time()<ct+300:
+                if not self.is_running():
+                    return
+            os.write(self.stdout[1], ("[TzGamePanel] 检测到实例关闭实例时长过长，已自动恢复到运行中状态\n").encode("utf-8"))
+            self.status = 1
+        threading.Thread(target=csis,kwargs={'self':self,'ct':time.time()}).start()
+        os.write(self.stdout[1], ("[TzGamePanel] 已发送停止命令："+stop_cmd+"，如果停止命令不正确将会无法正常停止\n").encode("utf-8"))
+        os.write(self.stdin[1],(stop_cmd+"\n").encode("utf-8"))
+
+    def kill(self):
+        if self.closed: 
+            raise Exception("此对象已被关闭")
+        if not self.is_running():
+            return False
+        self.proc.kill()
+        return True
+
+    def exec_cmd(self,cmd:str,log_to_terminal:bool=True):
+        if self.closed: 
+            raise Exception("此对象已被关闭")
+        if self.proc == None or self.proc.poll() != None:
+            return False
+        os.write(self.stdin[1],cmd.encode("utf-8"))
+        os.write(self.stdin[1],b"\n")
+        if log_to_terminal == True:
+            os.write(self.stdout[1],("[TzGamePanel] 用户执行了命令："+cmd+"\n").encode("utf-8"))
+        return True
+
+    def clear_log(self):
+        with open(os.path.abspath("data/InstanceLog/"+self.instance_id+".log"),"w") as f:
+            f.write("")
+        self.stdout2 = []
+
+    def close(self):
+        if self.closed:
+            return
+        self.closed = True
+        if self.proc != None and self.proc.poll() == None:
+            self.proc.kill()
+        for i in [self.stdin[0],self.stdin[1],self.stdout[0],self.stdout[1]]:
+            os.close(i)
+
+if __name__ == "__main__":
+    print("不要直接执行此文件！你需要执行TzGamePanel的main.py进行使用")
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import sys
 import os
 import platform
 from flask import Flask, request, make_response, abort
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room
 import hashlib
 import time
 import glob
 from subprocess import Popen, PIPE
 import psutil
+import threading
 
 VERSION = "1.0.0"
 sys_type = ""
@@ -22,10 +23,17 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 name_space = '/ws'
 
-started_instances = []
+instances = []
+
+tmp_terminal_token = []
+Thread_check_tmp_terminal_token = None
 
 os.chdir(os.path.dirname(__file__)) # 强制更改目录到项目所在目录
 from func import *
+
+def ret(status:int,msg:str,data=None):
+    return json.dumps({'status':status,'msg':msg,'data':data,'time':int(time.time())}), status
+
 def info():
     global sys_type,VERSION
     memory = psutil.virtual_memory()
@@ -41,16 +49,30 @@ def info():
     }
 
 def get_instances_list():
+    global instances
     files = glob.glob(os.path.join(os.path.dirname(__file__),"data","InstanceConfig","*.json"))
-    instances = []
+    instances2 = []
     for i in files:
         with open(i) as f:
             try:
                 inst = json.loads(f.read(9999999))
-                instances.append(inst)
+                instances2.append(inst)
             except json.decoder.JSONDecodeError:
                 continue
-    return instances
+    i = 0
+    while i<len(instances2):
+        for j in instances:
+            if j.instance_id == instances2[i]['id']:
+                instances2[i]['status'] = j.status
+                continue
+        i += 1
+    return instances2
+
+def get_instance(instance_id):
+    for i in get_instances_list():
+        if i['id'] == instance_id:
+            return i
+    return False
 
 def create_instance(name:str,start_cmd:str,stop_cmd:str="stop",instance_dir=None):
     instance_id = hashlib.md5(string=str(str(time.time())+"TzGamePanel Instance ID").encode("utf-8")).hexdigest()
@@ -61,8 +83,13 @@ def create_instance(name:str,start_cmd:str,stop_cmd:str="stop",instance_dir=None
         if not os.path.isdir(instance_dir):
             return {
                 'status': False,
-                'msg': "实例目录不存在"
+                'msg': "指定的实例目录不存在"
             }
+    if load_cmd_str(start_cmd) == False:
+        return {
+            'status': False,
+            'msg': "启动命令解析失败"
+        }
     instance_config_template = {
         "name": name,
         "id": instance_id,
@@ -76,7 +103,9 @@ def create_instance(name:str,start_cmd:str,stop_cmd:str="stop",instance_dir=None
     with open(os.path.abspath("data/InstanceConfig/"+instance_id+".json"),"w") as f:
         f.write(json.dumps(instance_config_template))
     with open(os.path.abspath("data/InstanceLog/"+instance_id+".log"),"w") as f:
-        f.write("实例已成功创建\nPowered by TzGamePanel\n")
+        f.write("[TzGamePanel] 实例已成功创建\nPowered by TzGamePanel\n")
+    global instances
+    instances.append(instance(cmd=load_cmd_str(start_cmd),cwd=instance_dir,instance_id=instance_id))
     return {
         'status': True,
         'msg': "OK",
@@ -84,33 +113,36 @@ def create_instance(name:str,start_cmd:str,stop_cmd:str="stop",instance_dir=None
     }
 
 def start_instance(instance_id:str):
+    global instances
     status = False
-    config = {}
-    for i in get_instances_list():
-        if i['id'] == instance_id:
+    inst = None
+    for i in instances:
+        if i.instance_id == instance_id:
             status = True
-            config = i['config']
+            inst = i
             break
     if status:
-        cmd = load_cmd_str(config['start_cmd'])
-        if cmd == False:
+        result = inst.start()
+        if result == True:
+            return {
+                'status': True,
+                'msg': "OK"
+            }
+        elif result == False:
             return {
                 'status': False,
-                'msg': "启动命令解析失败"
+                'msg': "实例正在运行中，无需再启动"
             }
-        start_inst = startinstance(cmd,config['work_directory'],instance_id)
+        else:
+            return {
+                'status': False,
+                'msy': "实例启动失败，请检查实例配置是否正确"
+            }
     else:
         return {
             'status': False,
             'msg': "没有此实例"
         }
-
-def ws_return(status:int=200,msg:str="OK",data=None):
-    return json.dumps({
-        "status": status,
-        "msg": msg,
-        "data": data
-    })
 
 @app.errorhandler(400)
 def e_400(e):
@@ -155,35 +187,35 @@ def daemon_is_ok():
 def get_info():
     token = request.args.get("token")
     if not token:
-        return json.dumps({'status': 403,'msg': "Permission denied"}), 403
+        return ret(403,"Permission denied")
     if not token == config['token']:
-        return json.dumps({'status': 403,'msg': "Pernission denied"}), 403
+        return ret(403,"Permission denied")
     else:
-        return json.dumps({'status': 200,'msg': "OK",'data':info()}), 200
+        return ret(200,"OK",info())
 
 @app.route("/get_instances", methods=['GET'])
 def get_instances():
     token = request.args.get("token")
     if not token:
-        return json.dumps({'status': 403,'msg': "Permission denied"}), 403
+        return ret(403,"Permission denied")
     if not token == config['token']:
-        return json.dumps({'status': 403,'msg': "Pernission denied"}), 403
+        return ret(403,"Permission denied")
     else:
-        return json.dumps({'status': 200,'msg': 'OK','data':get_instances_list()}), 200
+        return ret(200,"OK",get_instances_list())
 
 @app.route("/create_instance",methods=['POST'])
-def c_instance():
+def web_create_instance():
     token = request.args.get("token")
     if not token:
-        return json.dumps({'status': 403,'msg': "Permission denied"}), 403
+        return ret(403,"Permission denied")
     if not token == config['token']:
-        return json.dumps({'status': 403,'msg': "Pernission denied"}), 403
+        return ret(403,"Permission denied")
     else:
         data = request.get_json()
         if not data:
-            return json.dumps({'status': 400,'msg': "Empty JSON data"}), 400
+            return ret(400,"Empty JSON data")
         if data.get("name")==None or data.get("start_cmd")==None:
-            return json.dumps({'status': 400,'msg': "Missing parameter"}), 400
+            return ret(400,"Missing parameter")
         if data.get("stop_cmd") == None:
             stop_cmd = "stop"
         else:
@@ -191,22 +223,159 @@ def c_instance():
         instance_dir = data.get("instance_dir")
         result = create_instance(name=data.get("name"),start_cmd=data.get("start_cmd"),stop_cmd=stop_cmd,instance_dir=instance_dir)
         if not result['status']:
-            return json.dumps({'status': 400,'msg': "实例目录不存在"}), 400
+            return ret(400,"实例目录不存在")
         else:
-            return json.dumps({'status': 200,'msg': "OK",'instance_id':result['instance_id']}), 200
+            return ret(200,"OK",result['instance_id'])
+
+@app.route("/start_instance")
+def web_start_instance():
+    token = request.args.get("token")
+    if not token:
+        return ret(403,"Permission denied")
+    if not token == config['token']:
+        return ret(403,"Permission denied")
+    instance_id = request.args.get("instance_id")
+    if not instance_id:
+        return ret(400,"Missing parameter")
+    status = False
+    for i in get_instances_list():
+        if i['id'] == instance_id:
+            status = True
+            break
+    if status:
+        result = start_instance(instance_id)
+        if result['status']:
+            return ret(200,"OK")
+        else:
+            return ret(500,result['msg'])
+    else:
+        return ret(404,"实例不存在")
+
+@app.route("/stop_instance")
+def web_stop_instance():
+    global instances
+    token = request.args.get("token")
+    if not token:
+        return ret(403,"Permission denied")
+    if not token == config['token']:
+        return ret(403,"Permission denied")
+    instance_id = request.args.get("instance_id")
+    if not instance_id:
+        return ret(400,"Missing parameter")
+    status = False
+    inst = None
+    for i in instances:
+        if i.instance_id == instance_id:
+            inst = i
+            status = True
+            break
+    if status:
+        i.stop(get_instance(i.instance_id)['configs']['stop_cmd'])
+        return ret(200,"OK")
+    else:
+        return ret(404,"实例不存在")
+
+@app.route("/send_cmd_to_instance",methods=["POST"])
+def web_send_cmd_to_instance():
+    global instances
+    token = request.args.get("token")
+    if not token:
+        return ret(403,"Permission denied")
+    if not token == config['token']:
+        return ret(403,"Permission denied")
+    instance_id = request.args.get("instance_id")
+    data = request.get_json()
+    if not instance_id or not data.get("command"):
+        return ret(400,"Missing parameter")
+    if type(data.get("command")) != str:
+        return ret(400,"\"command\" must is str")
+    status = False
+    inst = None
+    for i in instances:
+        if i.instance_id == instance_id:
+            inst = i
+            status = True
+            break
+    if status:
+        result = i.exec_cmd(data.get("command"),log_to_terminal=data.get("log_to_terminal",True))
+        if result:
+            return ret(200,"OK")
+        else:
+            return ret(500,"实例未启动，无法执行命令")
+    else:
+        return ret(404,"实例不存在")
+
+@app.route("/get_tmp_terminal_connect_token",methods=["POST"])
+def gttct():
+    global tmp_terminal_token,Thread_check_tmp_terminal_token
+    if Thread_check_tmp_terminal_token == None:
+        def cttt():
+            global tmp_terminal_token
+            tmp_terminal_token2 = []
+            while True:
+                for i in tmp_terminal_token:
+                    if i['endtime'] >= int(time.time()):
+                        tmp_terminal_token2.append(i)
+                tmp_terminal_token = tmp_terminal_token2
+                tmp_terminal_token2 = []
+                time.sleep(1)
+        Thread_check_tmp_terminal_token = threading.Thread(target=cttt)
+        Thread_check_tmp_terminal_token.daemon = True
+        Thread_check_tmp_terminal_token.start()
+    token = request.args.get("token")
+    if not token:
+        return ret(403,"Permission denied")
+    if not token == config['token']:
+        return ret(403,"Permission denied")
+    data = request.get_json()
+    if not data:
+        return ret(400,"Empty JSON data")
+    if data.get("instance_id")==None:
+        return ret(400,"Missing parameter")
+    if get_instance(data.get("instance_id")) == False:
+        return ret(400,"实例不存在")
+    for i in tmp_terminal_token:
+        if i['instance_id'] == data.get("instance_id"):
+            return ret(200,"OK",{'token':i['token'],'instance_id':data.get("instance_id")})
+    token = hashlib.sha256(string=str(str(time.time())+"TzGamePanel Terminal Key").encode("utf-8")).hexdigest()
+    tmp_terminal_token.append({'instance_id':data.get("instance_id"),'token':token,'endtime':int(time.time())+86400})
+    return ret( 200, "OK", {'token':token,'instance_id':data.get("instance_id")} )
 
 
 @socketio.on("connect", namespace=name_space)
 def websocket_connect(ws):
     print("websocket建立连接")
 
-@socketio.on("my_event", namespace=name_space)
-def websocket_msg(msg):
-    print(msg)
-    emit('test',msg)
+@socketio.on("disconnect", namespace=name_space)
+def websocket_disconnect():
+    print("webscoket断开连接")
+
+@socketio.on("terminal", namespace=name_space)
+def websocket_terminal(msg):
+    global tmp_terminal_token
+    if type(msg) != dict:
+        emit("result",[False,-1,"Must is type 'dict'(or javascript object)."])
+        return
+    token = msg.get("token")
+    if token == None:
+        emit("result",[False,-1,"The parameter \"token\" was not found or it was \"null\"."])
+        return
+    status = False
+    instance_id = None
+    for i in tmp_terminal_token:
+        if i['token'] == msg.get("token"):
+            status = True
+            instance_id = i['instance_id']
+            break
+    if status:
+        emit("result",[True,0,"OK. Please listen event \"terminal\"."])
+    else:
+        emit("result",[False,-1,"Permission denied."])
+        return
+    join_room("instance_"+instance_id,namespace="/ws")
 
 def main():
-    global sys_type,config
+    global sys_type,config,instances
     print("_____     ____                      ____                  _\n|_   _|___/ ___| __ _ _ __ ___   ___|  _ \\ __ _ _ __   ___| |\n  | ||_  / |  _ / _` | '_ ` _ \\ / _ \\ |_) / _` | '_ \\ / _ \\ |\n  | | / /| |_| | (_| | | | | | |  __/  __/ (_| | | | |  __/ |\n  |_|/___|\\____|\\__,_|_| |_| |_|\\___|_|   \\__,_|_| |_|\\___|_|")
     print("___\n|   \\ __ _ ___ _ __  ___ _ _\n| |) / _` / -_) '  \\/ _ \\ ' \\\n|___/\\__,_\\___|_|_|_\\___/_||_|")
     if not os.path.exists("data"):
@@ -222,7 +391,7 @@ def main():
     token = None
     if not os.path.exists("data/configs/config.json"):
         with open("data/configs/config.json","w") as f:
-            token = hashlib.md5(string=str(str(time.time())+"TzGamePanel key").encode("utf-8")).hexdigest()
+            token = hashlib.md5(string=str(str(time.time())+"TzGamePanel Key").encode("utf-8")).hexdigest()
             f.write(json.dumps({
                 'host': '0.0.0.0',
                 'port': 8002,
@@ -233,31 +402,37 @@ def main():
     try:
         config = json.loads(config)
     except json.decoder.JSONDecodeError as err:
-        print(f"在加载配置文件时发生了错误：JSONDecodeError: {err}")
-        print("请尝试删除配置文件data/configs/config.json，然后再启动")
+        log(f"在加载配置文件时发生了错误：JSONDecodeError: {err}",loglevel="FATAL")
+        log("请尝试删除配置文件data/configs/config.json，然后再启动",loglevel="FATAL")
         sys.exit(1)
     if token != None:
-        print("第一次启动，已生成token: "+token)
+        log("第一次启动，已生成token: "+token)
     if sys.platform.startswith("linux"):
         sys_type = "Linux"
     elif sys.platform.startswith("win"):
         sys_type = "Windows"
     else:
-        print("不支持你的操作系统！")
-        print(f"sys.platform = {platform}")
+        log("不支持你的操作系统！",loglevel="ERROR")
+        log(f"sys.platform = {platform}",loglevel="ERROR")
         sys.exit(1)
-    print("VERSION: "+str(VERSION))
-    print("此设备的平台："+sys.platform)
-    print("此设备的操作系统："+sys_type)
-    print("TzGamePanel开源免费，遵循GPLv3开源许可证，项目地址：https://gitee.com/tzdtwsj/TzGamePanel-Daemon")
-    print(f"TzGamePanel监听在地址{config['host']}，端口{config['port']}")
-    print("此进程的PID是"+str(os.getpid()))
-    print("退出请按下Ctrl + C")
+    log("守护进程版本: "+str(VERSION))
+    log("此设备的平台："+sys.platform)
+    log("此设备的操作系统："+sys_type)
+    log("TzGamePanel开源免费，遵循GPLv3开源许可证，项目地址：https://gitee.com/tzdtwsj/TzGamePanel-Daemon")
+    log("加载实例中")
+    for i in get_instances_list():
+        instances.append(instance(cmd=load_cmd_str(i['configs']['start_cmd']),cwd=i['configs']['work_directory'],instance_id=i['id']))
+    log("加载实例完成")
+    log(f"TzGamePanel监听在地址{config['host']}，端口{config['port']}")
+    log("此进程的PID是"+str(os.getpid()))
+    log("退出请按下Ctrl + C")
     socketio.run(app,host=config['host'],port=config['port'])
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("正在退出")
+        log("正在退出")
+        for i in instances:
+            i.kill()
         sys.exit(0)
